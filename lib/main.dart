69f643546382d8659e5735489f1580c2f86739eb
Pedro Mendes <pedrohm441@gmail.com>
2023-11-10 21:25:57 -0300

    linkando tela home com a tela detalhe

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: AdicionarMangaScreen(),
+        body: HomeScreen(),
       ),
     );
   }
