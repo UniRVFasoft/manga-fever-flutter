0d69da4bbe793943b66883190fd9e8e65de10db8
Pedro Mendes <pedrohm441@gmail.com>
2023-12-05 19:24:08 -0300

    Teste de teste

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,23 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:mangafaver/main.dart';
+import 'package:mangafaver/screens/HomeScreen.dart'; // Importe o arquivo HomeScreen.dart
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+  testWidgets('Verifica se a lista está sendo exibida',
+      (WidgetTester tester) async {
+    // Cria o widget que contém a tela HomeScreen
+    await tester.pumpWidget(
+      MaterialApp(
+        home: HomeScreen(
+            searchTerm:
+                ''), // Inicializa a tela HomeScreen com um searchTerm vazio
+      ),
+    );
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Aguarda a renderização dos widgets
+    await tester.pumpAndSettle();
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verifica se o widget de lista está presente na tela
+    expect(find.byType(GridView), findsOneWidget);
   });
 }
